21aa3a29ad46ae2ec1b857c8b303c8631be1c4f9
Yaniv Or <yaniv.or.78@gmail.com>
2023-03-16 07:20:32 +0200

    welcome and login

---
+++ src/views/Welcome.tsx
@@ -0,0 +1,36 @@
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
+const Welcome = () => {
+  const nav = useNavigate();
+
+  const [email, setEmail] = useState('yaniv.or.78@gmail.com');
+  const [nickname, setNickname] = useState('YanivOr');
+
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <input
+        type='text'
+        placeholder='enter email'
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+      />
+      <input
+        type='text'
+        placeholder='enter nickname'
+        value={nickname}
+        onChange={(event) => setNickname(event.target.value)}
+      />
+      <button
+        onClick={() => {
+          nav('/chat');
+        }}
+      >
+        Send
+      </button>
+    </div>
+  );
+};
+
+export default Welcome;
